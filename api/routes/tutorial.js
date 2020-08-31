1a7912a3ca9ae1bbe3abbf35b5e12d380259e4d0
vikmanatus <v.stoyanov@eurelis.com>
2020-08-31 10:42:07 +0200

    basic setup

--- api/routes/tutorial.js
+++ api/routes/tutorial.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const tf = require('@tensorflow/tfjs');
+
+// Optional Load the binding:
+// Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+require('@tensorflow/tfjs-node');
 
 router.get('/', (req,res)=>{
     return res.status(200).json({message:'Welcolme to the tutorial route'});
