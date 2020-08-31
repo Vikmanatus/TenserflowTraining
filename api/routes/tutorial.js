3a826492b513e3f0acc035fb5c2ecd2c8077788d
vikmanatus <v.stoyanov@eurelis.com>
2020-08-31 11:16:14 +0200

    Some other 5x3 tensor created

--- api/routes/tutorial.js
+++ api/routes/tutorial.js
@@ -8,11 +8,20 @@ require('@tensorflow/tfjs-node');
 
 // To create a tensor ==> Values, shapes, dtype (go to documentation for more information)
 router.get('/', (req,res)=>{
+
                                     // Values                   // Shapes x^n
     const firstTenser = tf.tensor([0, 0, 127,255,100,50,24,54],[2,2,2]);
     //firstTenser.print()
-    console.log(firstTenser)
-    
+    console.log(firstTenser);
+
+    // Creating 5 x 3 tensor
+    const values = [];
+    for (let i =0; i<15; i++){
+        values[i] = Math.random(0,100);
+    }
+    const shape = [5,3];
+    const data = tf.tensor(values,shape);
+    console.log('DATA: ', data.print());
     return res.status(200).json({message:'Welcolme to the tutorial route'});
 });
 
