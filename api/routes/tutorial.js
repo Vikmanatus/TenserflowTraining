1abebabc935384cd74d0a5202dc98c57957a6ea0
vikmanatus <v.stoyanov@eurelis.com>
2020-08-31 11:08:13 +0200

    some basic tensor added

--- api/routes/tutorial.js
+++ api/routes/tutorial.js
@@ -6,7 +6,13 @@ const tf = require('@tensorflow/tfjs');
 // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
 require('@tensorflow/tfjs-node');
 
+// To create a tensor ==> Values, shapes, dtype (go to documentation for more information)
 router.get('/', (req,res)=>{
+                                    // Values                   // Shapes x^n
+    const firstTenser = tf.tensor([0, 0, 127,255,100,50,24,54],[2,2,2]);
+    //firstTenser.print()
+    console.log(firstTenser)
+    
     return res.status(200).json({message:'Welcolme to the tutorial route'});
 });
 
